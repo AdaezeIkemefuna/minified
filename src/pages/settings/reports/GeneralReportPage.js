434331745c82979c0725591c67c4ae82de2de569
Adaeze Ikemefuna <hamsaikemefuna@gmail.com>
2022-10-07 13:22:04 +0100

    Update GeneralReportPage.js

--- src/pages/settings/reports/GeneralReportPage.js
+++ src/pages/settings/reports/GeneralReportPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import AuthContext from '../../../context/AuthContext'
 
 
@@ -8,7 +8,18 @@ function GeneralReportPage({ report, total, bar, lounge, barTotal, loungeTotal})
   const {adminCashPayments,
     adminPosPayments,
     adminTransferPayments,
-    adminTotalRevenue,} = useContext(AuthContext);
+    adminTotalRevenue, user, displayAdminTables, displayTables} = useContext(AuthContext);
+
+    const activeUser = user.username;
+    const activePasscode = user.passcode;
+    useEffect(() => {
+      if (user.role === "Super Admin") {
+        displayAdminTables(activeUser, activePasscode);
+      } else {
+        displayTables(activeUser);
+      }
+    }, [activeUser, activePasscode]);
+
 
 const current = new Date();
  const date = `${current.toLocaleString("en-US", {
@@ -19,17 +30,20 @@ const current = new Date();
 
   return (
     <div  style={{background:"white"}}>
-               <h1 style={{ fontSize: "2rem", textAlign:"center", paddingTop:"3rem"}}>General Report</h1>
+                <div style={{display:"flex", justifyContent:"space-between"}}>
+                <h1 style={{ fontSize: "2rem", margin:"0 auto", paddingTop:"3rem",justifySelf:"center"}}>General Report</h1>
+                <div style={{fontSize:"1.5rem", paddingTop:"3rem", textDecoration:"underline"}}>{date}</div>
+                </div>
+              
               <div style={{display:"flex", justifyContent:"space-between", paddingTop:"5rem", paddingBottom:"1rem"}}>
               <div style={{display:"flex", flexDirection:"column", gap:"2rem", paddingLeft:'2rem'}}>
               <div style={{fontSize:"1.2rem"}}><b><span>Bar Total:</span> N{barTotal.toLocaleString("en-US")}</b></div>
               <div style={{fontSize:"1.2rem"}}><b><span>Lounge Total:</span> N{loungeTotal.toLocaleString("en-US")}</b></div>
               <div style={{fontSize:"1.2rem"}}><b><span>Total Revenue: </span>N{adminTotalRevenue.toLocaleString("en-US")} </b></div>
-              <div style={{fontSize:"1.2rem"}}><b><span>Cash Payments:</span> N{adminCashPayments.toLocaleString("en-US")} </b></div>
                 </div>
  
               <div style={{display:"flex", flexDirection:"column", gap:"2rem", paddingRight:'2rem'}}>
-              <div style={{fontSize:"1.2rem"}}><b>{date}</b></div>
+              <div style={{fontSize:"1.2rem"}}><b><span>Cash Payments:</span> N{adminCashPayments.toLocaleString("en-US")} </b></div>
               <div style={{fontSize:"1.2rem"}}><b><span>POS Payments:</span> N{adminPosPayments.toLocaleString("en-US")} </b></div>
               <div style={{fontSize:"1.2rem"}}><b><span>Transfer Payments:</span> N{adminTransferPayments.toLocaleString("en-US")}</b></div>
 
@@ -40,7 +54,7 @@ const current = new Date();
       <div >
         <h1 style={{textAlign:"center", marginTop:"2rem", paddingBottom:"1rem"}}>Bar</h1>
         <div className="table__header report__table">
-          <div  className="table__row ">Description</div>
+          <div  className="table__row ">Desc</div>
           <div className="table__row">Price</div>
           <div className="table__row">Quantity</div>
           <div className="table__row">Sub-Total</div>
@@ -59,7 +73,7 @@ const current = new Date();
       <div >
         <h1 style={{textAlign:"center", marginTop:"2rem", paddingBottom:"1rem"}}>Lounge</h1>
         <div className="table__header report__table">
-          <div  className="table__row">Description</div>
+          <div  className="table__row">Desc</div>
           <div className="table__row">Price</div>
           <div className="table__row">Quantity</div>
           <div className="table__row">Sub-Total</div>
